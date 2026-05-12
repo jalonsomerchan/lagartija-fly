01a1cb253c42e0148d5e4425e8c7472963e2f75f
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-12 17:48:25 +0200

    Wire game modules from entrypoint

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,15 @@
 import '../css/main.css';
+import {Game} from './game.js';
+import {ui} from './ui.js';
 
-console.info('JS Template initialized');
+const view=ui();
+const game=new Game(document.getElementById('gameCanvas'),view);
+
+view.play.addEventListener('click',view.showLevels);
+view.levels.addEventListener('click',e=>{
+  const level=e.target?.dataset?.level;
+  if(level)game.start(level);
+});
+
+document.addEventListener('gesturestart',e=>e.preventDefault());
+document.addEventListener('dblclick',e=>e.preventDefault(),{passive:false});
